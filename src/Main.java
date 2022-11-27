9dfce6c4d9f8f44bb69a4465b6ecaff04a564f0a
Handsken <nan.222@yandex.ru>
2022-11-27 16:12:24 +0500

    начало

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int i =0;
+        int m=0;
+        while (i<2459000){
+            i+=15000;
+            m+=1;
+        }
+        System.out.print("Чтобы накопить 2459000 нам потребуется "+m);
+        System.out.print(" Месяца");
+    }
+}
