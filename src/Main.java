397ca3d05f98731ac2beb0ea155ebb660b26caf8
Handsken <nan.222@yandex.ru>
2022-11-27 16:32:46 +0500

    задание 2 end

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+        System.out.println("Задание 1");
         int i =0;
         int m=0;
         while (i<2459000){
             i+=15000;
             m+=1;
+            System.out.print(m);
+            System.out.print(" Сумма накоплений равна "+i);
+            System.out.println(" Рублей");
         }
-        System.out.print("Чтобы накопить 2459000 нам потребуется "+m);
-        System.out.print("Месяца");
+        // Задание 2
+        System.out.println("Задание 2");
+        int w=0;
+        int f=9;
+        while (w<10){
+            w+=1;
+            System.out.print(" "+w);
+        }
+        System.out.println();
+        for (f=10; f>=1;f--) {
+            System.out.print(" "+f);
+        }
+        //Задание 3
     }
 }
